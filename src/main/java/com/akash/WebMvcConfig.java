fad928c442fd9754a52347a83c3c387fd319f40c
EnnBi <nadym.baba@gmail.com>
2022-02-19 10:02:03 +0530

    Removed Interceptor

--- src/main/java/com/akash/WebMvcConfig.java
+++ src/main/java/com/akash/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new MyInterceptor());
+		//registry.addInterceptor(new MyInterceptor());
 	}
 }
 
